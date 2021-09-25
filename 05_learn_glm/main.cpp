3ce18327ac9f909d6777cf04f2a10e68fba8ac7c
wlfrii <longfei.wang@sjtu.edu.cn>
2021-09-25 11:49:00 +0800

    add learn-glm

---
+++ 05_learn_glm/main.cpp
@@ -0,0 +1,106 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "../gll_util/gll_util.h"
+#include <glm/glm.hpp>
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+int main(int argc, char* argv[])
+{
+    gll::initGLFW();
+    GLFWwindow* window = gll::createGLFWwindow(SCR_WIDTH, SCR_HEIGHT);
+    if (window == nullptr) {
+        return -1;
+    }
+    if(!gll::initGLAD())
+        return -2;
+
+    gll::Shader myshader;
+    myshader.load("../textures/05.1.texture.vs", "../textures/05.1.texture.fs");
+
+    float vertices[] = {
+        // positions         // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+       -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
+    };
+    unsigned int indices[] = {  
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    gll::VAVBEBO vavbebo;
+    vavbebo.bind(vertices, sizeof(vertices), indices, sizeof(indices));
+
+    // ------------------------------------------------------------------------
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    // load and create a texture 
+    // -------------------------
+    unsigned int texture;
+    if( !gll::create2DTexture("../resources/container.jpg", texture) ){
+        return -2;
+    }
+    unsigned int texture2;
+    if( !gll::create2DTexture("../resources/awesomeface.png", texture2, GL_RGBA)) {
+        return -2;
+    }
+    //tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+    myshader.use(); // don't forget to activate/use the shader before setting uniforms!
+    myshader.setInt("texture1", 0);
+    myshader.setInt("texture2", 1);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        gll::processInput(window);
+        // render
+        gll::render();
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        glm::mat4 trans(1.0);
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+        myshader.setMat4f("transform", trans);
+        
+        // render container
+        myshader.use();
+        vavbebo.bindVertexArray();
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, glm::vec3(-0.5, 0.5, 0.0));
+        float scale = sin(glfwGetTime());
+        trans = glm::scale(trans, glm::vec3(scale, scale, scale));
+        myshader.setMat4f("transform", trans);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    vavbebo.release();
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
+}
--- gll_util/gll_shader.h
+++ gll_util/gll_shader.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include <cstdio>
+#include <glm/glm.hpp>
 
 namespace gll{
 
@@ -121,6 +122,11 @@ public:
     void setFloat(const std::string &name, float value) const
     { 
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
+    }
+
+    void setMat4f(const std::string &name, const glm::mat4 &mat) const
+    {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
     }
 
     void release()
