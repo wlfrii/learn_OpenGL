eb713b91b07e95b2885ee93d28547d0b125ef577
wlfrii <longfei.wang@sjtu.edu.cn>
2022-04-28 23:30:33 +0800

    modified based on gl_util

--- 01_hello_window/main.cpp
+++ 01_hello_window/main.cpp
@@ -1,30 +1,34 @@
-#include "../gll_util/gll_util.h"
+#include "../gl_util/gl_window.h"
+#include <iostream>
 
+std::string proj_name = "01_hello_window";
 
-int main()
+int main(int argc, char* argv[])
 {
-    gll::initGLFW();
-    GLFWwindow* window = gll::createGLFWwindow();
-    if(!window) 
-        return -2;  
-    if( !gll::initGLAD() ) 
-        return -1;
+    gl_util::Window window(800, 600);
 
-    glViewport(0, 0, 800, 600);
-
-    while(!glfwWindowShouldClose(window)){
-        // Input
-        gll::processInput(window);
-        // Render
-        gll::render();
-
-        // Check the mouse/keyboard events
-        glfwPollEvents();
-        // Store the color of each pixel in GLFW window
-        glfwSwapBuffers(window);
+    // --------------- Prase inputs ---------------
+    if (argc <= 1){
+        std::cout << proj_name << ":\n\tYou can input 3 or 4 integer to specify the background color of the window.\n" << "\t# 3 integer for RGB\n" << "\t# 4 integer for RGBA\n";
     }
+    else if(argc >= 4){
+        unsigned char R = std::stoi(argv[1]);
+        unsigned char G = std::stoi(argv[2]);
+        unsigned char B = std::stoi(argv[3]);
+        unsigned char A = 255;
+        if(argc == 5) A = std::stoi(argv[4]);
 
-    glfwTerminate();
+        window.setBackgroundColor(R,G,B,A);
+    }
+    // ---------------------------------------------
+
+
+    // Window show
+    while(!window.shouldClose()){
+        window.activate();
+        window.refresh();
+    }
+    window.release();
 
     return 0;
 }
